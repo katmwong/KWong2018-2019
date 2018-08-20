28f198c256d04304c8120a70930cadac8f8e730c
APCS3rd <APCS3rd@MHS-Math2-23>
2018-08-20 10:44:32 -0700

    First commit to github

---
+++ Hello First Coding Project/src/Hello_world.java
@@ -0,0 +1,19 @@
+/**
+ * *Prints message in console
+ * Write a description of class Hello_world here.
+ * 
+ * @author Katrina
+ * @version 1
+ */
+public class Hello_world 
+{
+	  public static void main(String[] args)
+	    {
+	        System.out.println("Hello World! From <Katrina>");
+	        System.out.println("how are you");
+//Display the string
+	   }
+}
+
+
+
